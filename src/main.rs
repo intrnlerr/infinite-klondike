70352a2f6d1b0d6b1231169939c1f521f441a96d
intrnlerr <nulltestb1@gmail.com>
2024-01-14 23:23:58 +0000

    do funny bit things

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::collections::HashMap;
 
-use ::rand::{distributions::Standard, prelude::Distribution, Rng};
+use ::rand::{distributions::Standard, prelude::Distribution};
+use cards::{BitCard, CardStack, Column};
 use macroquad::prelude::*;
+
+mod cards;
 
 fn window_conf() -> Conf {
     Conf {
@@ -45,10 +48,10 @@ fn draw_atlas_item(atlas: Texture2D, x: f32, y: f32, offset: f32) {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 enum Suit {
-    Club,
-    Diamond,
-    Heart,
-    Spade,
+    Club = 0b10,
+    Diamond = 0b00,
+    Heart = 0b01,
+    Spade = 0b11,
 }
 
 impl Suit {
@@ -58,18 +61,6 @@ impl Suit {
             Suit::Diamond => 352.0,
             Suit::Heart => 374.0,
             Suit::Spade => 396.0,
-        }
-    }
-    const fn is_red(&self) -> bool {
-        match self {
-            Suit::Club | Suit::Spade => false,
-            Suit::Diamond | Suit::Heart => true,
-        }
-    }
-    const fn get_color(&self) -> Color {
-        match self {
-            Suit::Club | Suit::Spade => WHITE,
-            Suit::Diamond | Suit::Heart => RED,
         }
     }
 }
@@ -85,70 +76,30 @@ impl Distribution<Suit> for Standard {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-struct Pip(u8);
-
-impl Pip {
-    const fn is_ace(self) -> bool {
-        self.0 == 0
-    }
-    fn get_x(&self) -> f32 {
-        44.0 + 22.0 * self.0 as f32
-    }
-    fn can_drop_on(&self, other: &Pip) -> bool {
-        self.0 + 1 == other.0
-    }
-}
-
-#[derive(Debug, Clone)]
-struct Card {
-    suit: Suit,
-    inner: Pip,
-}
-
-impl Distribution<Card> for Standard {
-    fn sample<R: ::rand::Rng + ?Sized>(&self, rng: &mut R) -> Card {
-        Card {
-            suit: rng.gen(),
-            inner: Pip(rng.gen_range(0..13)),
-        }
-    }
-}
-
-impl Card {
-    fn draw(&self, atlas: Texture2D, x: f32, y: f32) {
-        let color = self.suit.get_color();
-        draw_texture_box(atlas, x, y, WHITE, Rect::new(0.0, 0.0, 22.0, 32.0));
-        draw_texture_box(
-            atlas,
-            x,
-            y,
-            color,
-            Rect::new(self.suit.get_x(), 0.0, 22.0, 32.0),
-        );
-        draw_texture_box(
-            atlas,
-            x,
-            y,
-            color,
-            Rect::new(self.inner.get_x(), 0.0, 22.0, 32.0),
-        );
-    }
-    fn can_drop_on(&self, other: &Card) -> bool {
-        self.inner.can_drop_on(&other.inner) && self.suit.is_red() != other.suit.is_red()
-    }
-}
-
-struct Stack {
-    visible: Vec<Card>,
-    under: u32,
+fn draw_card(card: BitCard, atlas: Texture2D, x: f32, y: f32) {
+    let color = if card.is_red() { RED } else { WHITE };
+    draw_texture_box(atlas, x, y, WHITE, Rect::new(0.0, 0.0, 22.0, 32.0));
+    draw_texture_box(
+        atlas,
+        x,
+        y,
+        color,
+        Rect::new(card.suit().get_x(), 0.0, 22.0, 32.0),
+    );
+    draw_texture_box(
+        atlas,
+        x,
+        y,
+        color,
+        Rect::new(44.0 + 22.0 * card.number() as f32, 0.0, 22.0, 32.0),
+    );
 }
 
 struct State {
-    grabbed_stack: Vec<Card>,
+    grabbed_stack: CardStack,
     grabbed_stack_row: usize,
-    tableau: Vec<Stack>,
-    foundations: HashMap<usize, Card>,
+    tableau: Vec<Column>,
+    foundations: HashMap<usize, BitCard>,
     camera: Vec2,
 }
 
@@ -160,18 +111,14 @@ impl State {
         let mut tableau = Vec::new();
         let mut rng = ::rand::thread_rng();
         for x in 0..50 {
-            let stack = Stack {
-                visible: vec![rng.gen()],
-                under: x,
-            };
-            tableau.push(stack)
+            tableau.push(Column::new(&mut rng, x))
         }
 
         let w = screen_width();
         let shown_cards = 7.0;
         let camera = Vec2::new(w - (shown_cards - 1.0) * 48.0, 2.0);
         State {
-            grabbed_stack: Vec::new(),
+            grabbed_stack: CardStack::empty(),
             tableau,
             foundations: HashMap::new(),
             grabbed_stack_row: 0,
@@ -205,7 +152,7 @@ impl State {
                     22.0,
                 )
             }
-            if stack.under == 0 && stack.visible.is_empty() {
+            if stack.under == 0 && stack.is_visible_empty() {
                 // draw empty
                 draw_atlas_item(
                     atlas,
@@ -214,8 +161,9 @@ impl State {
                     418.0,
                 )
             } else {
-                for (n, card) in stack.visible.iter().enumerate() {
-                    card.draw(
+                for (n, card) in stack.visible().iter().enumerate() {
+                    draw_card(
+                        card,
                         atlas,
                         48.0 * (x as f32 - 1.0) + camera_offset_x,
                         16.0 * (n + stack.under as usize) as f32
@@ -235,7 +183,8 @@ impl State {
         for x in foundation_min..visible {
             let local_x = x - foundation_min;
             if let Some(card) = self.foundations.get(&x) {
-                card.draw(
+                draw_card(
+                    *card,
                     atlas,
                     48.0 * (local_x as f32 - 1.0) + foundation_camera_x_offset,
                     self.camera.y,
@@ -253,7 +202,7 @@ impl State {
             let (x, y) = mouse_position();
             let x = (x / 2.0).floor() * 2.0;
             let y = (y / 2.0).floor() * 2.0;
-            card.draw(atlas, x, y + (16.0 * n as f32));
+            draw_card(card, atlas, x, y + (16.0 * n as f32));
         }
         // debug!("{:?}", Self::get_row_over_mouse());
     }
@@ -262,6 +211,19 @@ impl State {
         let y = y - self.camera.y;
         y < Self::TABLEAU_Y_OFFSET
     }
+
+    /// this finalizes an card move from a column.
+    /// this reveals a new card if the moves leaves the "visible" stack empty
+    /// and there are hidden cards.
+    fn finalize_column(&mut self) {
+        self.tableau[self.grabbed_stack_row].maybe_reveal_card(&mut ::rand::thread_rng())
+    }
+
+    /// return the grabbed cards to the original column
+    fn reset_column(&mut self) {
+        self.tableau[self.grabbed_stack_row].append(&mut self.grabbed_stack)
+    }
+
     fn on_click(&mut self) {
         if self.grabbed_stack.is_empty() {
             // nothing grabbed
@@ -273,14 +235,14 @@ impl State {
                     y.checked_sub(self.tableau[row_over].under.try_into().unwrap())
                 {
                     self.grabbed_stack_row = row_over;
-                    if visible_idx >= self.tableau[row_over].visible.len() {
+                    if visible_idx >= self.tableau[row_over].visible().len().into() {
                         // only pickup the top card
-                        if let Some(card) = self.tableau[row_over].visible.pop() {
+                        if let Some(card) = self.tableau[row_over].visible_mut().pop() {
                             self.grabbed_stack.push(card);
                         }
                     } else {
                         self.grabbed_stack
-                            .extend(self.tableau[row_over].visible.drain(visible_idx..))
+                            .take_from(self.tableau[row_over].visible_mut(), visible_idx);
                     }
                 }
             }
@@ -288,72 +250,34 @@ impl State {
             // drop grabbed stack on other stack
             if let Some(row_over) = self.get_row_over_mouse() {
                 if self.is_mouse_on_foundation() && self.grabbed_stack.len() == 1 {
-                    let grabbed_card = &self.grabbed_stack[0];
+                    let grabbed_card = self.grabbed_stack.top();
                     if let Some(foundation_index) = row_over.checked_sub(3) {
                         if let Some(card) = self.foundations.get(&foundation_index) {
-                            if card.suit == grabbed_card.suit
-                                && card.inner.0 + 1 == grabbed_card.inner.0
-                            {
+                            if card.same_suit(grabbed_card) && grabbed_card.is_next_card(*card) {
                                 self.foundations
                                     .insert(foundation_index, self.grabbed_stack.pop().unwrap());
-                                if self.tableau[self.grabbed_stack_row].under > 0
-                                    && self.tableau[self.grabbed_stack_row].visible.is_empty()
-                                {
-                                    self.tableau[self.grabbed_stack_row]
-                                        .visible
-                                        .push(::rand::random());
-                                    self.tableau[self.grabbed_stack_row].under -= 1;
-                                }
+                                self.finalize_column()
                             }
-                        } else {
-                            if grabbed_card.inner.is_ace() {
-                                self.foundations
-                                    .insert(foundation_index, self.grabbed_stack.pop().unwrap());
-                                if self.tableau[self.grabbed_stack_row].under > 0
-                                    && self.tableau[self.grabbed_stack_row].visible.is_empty()
-                                {
-                                    self.tableau[self.grabbed_stack_row]
-                                        .visible
-                                        .push(::rand::random());
-                                    self.tableau[self.grabbed_stack_row].under -= 1;
-                                }
-                            }
+                        } else if grabbed_card.is_ace() {
+                            self.foundations
+                                .insert(foundation_index, self.grabbed_stack.pop().unwrap());
+                            self.finalize_column()
                         }
                     } else {
-                        self.tableau[self.grabbed_stack_row]
-                            .visible
-                            .append(&mut self.grabbed_stack)
+                        self.reset_column()
                     }
                 } else {
                     let stack = &mut self.tableau[row_over];
-                    if self
-                        .grabbed_stack
-                        .first()
-                        .zip(stack.visible.last())
-                        .map(|(grabbed, target)| grabbed.can_drop_on(target))
-                        .unwrap_or(false)
-                        || stack.under == 0 && stack.visible.is_empty()
-                    {
-                        stack.visible.append(&mut self.grabbed_stack);
+                    if stack.visible().can_stack(self.grabbed_stack.top()) {
+                        stack.append(&mut self.grabbed_stack);
                         // success, deal with the grabbed stack
-                        if self.tableau[self.grabbed_stack_row].under > 0
-                            && self.tableau[self.grabbed_stack_row].visible.is_empty()
-                        {
-                            self.tableau[self.grabbed_stack_row]
-                                .visible
-                                .push(::rand::random());
-                            self.tableau[self.grabbed_stack_row].under -= 1;
-                        }
+                        self.finalize_column()
                     } else {
-                        self.tableau[self.grabbed_stack_row]
-                            .visible
-                            .append(&mut self.grabbed_stack)
+                        self.reset_column()
                     }
                 }
             } else {
-                self.tableau[self.grabbed_stack_row]
-                    .visible
-                    .append(&mut self.grabbed_stack)
+                self.reset_column()
             }
         }
     }
@@ -361,11 +285,10 @@ impl State {
         let w = screen_width();
         let visible = (w - self.camera.x) as usize / 48 + 2;
         if visible > self.tableau.len() {
+            let mut rng = ::rand::thread_rng();
             for height in self.tableau.len()..visible {
-                self.tableau.push(Stack {
-                    visible: vec![::rand::random()],
-                    under: height as u32,
-                })
+                self.tableau
+                    .push(Column::new(&mut rng, height.try_into().unwrap()))
             }
         }
     }
@@ -373,7 +296,9 @@ impl State {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let atlas = load_texture("cards.png").await.expect("could not find cards.png");
+    let atlas = load_texture("cards.png")
+        .await
+        .expect("could not find cards.png");
     atlas.set_filter(FilterMode::Nearest);
     let mut state = State::new();
     let mut old_pos = mouse_position();
